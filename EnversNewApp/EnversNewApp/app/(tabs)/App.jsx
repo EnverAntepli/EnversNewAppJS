4bfd0c36ea69e890a9c8bcf90de7383bfe091771
Enver Emir Antepli <anantepli@gmail.com>
2024-05-28 20:46:53 -0600

    Update App.jsx

--- EnversNewApp/EnversNewApp/app/(tabs)/App.jsx
+++ EnversNewApp/EnversNewApp/app/(tabs)/App.jsx
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View ,SafeAreaView} from 'react-native'
 import React from 'react'
-
-const App = () => {
+ 
+ 
+const DATA=[
+{id:"1",title:"List1"},
+{id:"2",title:"List2"},
+{id:"3",title:"List3"},
+{id:"4",title:"List4"},
+{id:"5",title:"List5"},
+{id:"6",title:"List6"},
+{id:"7",title:"List7"},
+{id:"8",title:"List8"},
+{id:"9",title:"List9"},
+{id:"10",title:"List10"},
+{id:"11",title:"List11"},
+ 
+];
+const Item=({title})=>{
+return(
+<View style={styles.item}>
+  <Text style={styles.title}>{title}</Text>
+</View>
+ 
+);
+ 
+};
+const app = () => {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
-  )
-}
-
-export default App
-
-const styles = StyleSheet.create({})
+    <SafeAreaView>
+      <FlatList data={DATA} renderItem={({item})=><Item title={item.title}/>}
+        keyExtractor={item=>item.id}
+        />
+    </SafeAreaView>
+  );
+};
+ 
+export default app;
+ 
+const styles = StyleSheet.create({
+container:{
+flex:1,
+margintop:20,
+},
+item:{
+backgroundColor:'gray',
+padding:20,
+marginHorizontal:20,
+marginVertical:20,
+},
+title:{
+fontsize:32,
+},
+ 
+ 
+ 
+ 
+});
